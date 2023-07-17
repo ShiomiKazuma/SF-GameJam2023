8640789f4c03e3ca579aa8a2feff6b61676c6c76
ueno <134473167+r1noel@users.noreply.github.com>
2023-07-17 16:40:03 +0900

    [add]イラストの追加とフォルダ分け

---
+++ Assets/GameJamPackages/ueno/Mask/LifeGauge.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeGauge : MonoBehaviour
+{
+    [SerializeField] Slider _slider;
+    [SerializeField] PlayerHp _player;
+    //public void Damaged()
+    //{
+    //    _slider.value = _player._curentHp;
+    //}
+
+    //void Start()
+    //{
+    //    _slider.maxValue = _player._maxHp;
+    //    _slider.value = _player._curentHp;
+    //}
+    public void Update()
+    {
+        _slider.value = _player._curentHp / _player._maxHp;
+    }
+}
