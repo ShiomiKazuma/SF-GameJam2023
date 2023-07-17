9e7ecee6501e6aba198e636846ab4edd4500a24b
yuukobuchi <134473501+yuukobuchi@users.noreply.github.com>
2023-07-17 11:09:49 +0900

    [add]Enemyの死亡アニメーションの追加、マージ用Scene作成

--- Assets/GameJamPackages/Kobuchi/Script/E_Health.cs
+++ Assets/GameJamPackages/Kobuchi/Script/E_Health.cs
@@ -5,21 +5,33 @@ public class E_Health : MonoBehaviour
     [SerializeField] private float _initHealth;
     [SerializeField] private string _bulletTag;
     
-    private float _health;
+    public float _health;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        _animator = GetComponent<Animator>();
+
         _health = _initHealth;
+    }
+
+    private void Update()
+    {
+        if (_health <= 0) DeadAnim();
     }
 
     public void EnemyDamaged(float damage)
     {
         _health -= damage;
-        if (_health <= 0) Destroy();
     }
 
-    private void Destroy()
+    private void DeadAnim()
+    {
+        _animator.SetTrigger("IsDead");
+    }
+
+    private void EndDeadAnim()
     {
         Destroy(this.gameObject);
     }
